18c7b0b0b3cdc1ffd9f8879212a2e98f412c5aa3
sarkarusama <sheikhusama8017@gmail.com>
2025-01-24 19:49:13 +0500

    link change

--- components/top_categories.tsx
+++ components/top_categories.tsx
@@ -1,125 +1,76 @@
-"use client";
-import Image from "next/image";
-import React from "react";
 
-const Top_categories = () => {
+"use client"
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { client } from "@/sanity/lib/client";
+import Image from "next/image";
+
+type Category = {
+  _id: string;
+  title: string;
+  imageUrl: string;
+  products:number
+};
+
+const CategoryListPage = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  const fetchCategories = async () => {
+    try {
+      const result = await client.fetch(`
+        *[_type == "categories"]{
+          _id,
+          title,
+          'imageUrl': image.asset->url,
+          products
+          
+        }
+      `);
+
+      setCategories(result);
+    } catch (error) {
+      console.error("Error fetching categories:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   return (
     <section>
-  <div className="px-4">
-    <h2 className="max-w-screen-xl mx-auto text-3xl font-bold mt-20 mb-10 text-[#272343] text-center lg:text-start ">
-      Top Categories
-    </h2>
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto max-w-screen-xl">
-      {/* Card 1 */}
-      <div className="relative">
-        <Image
-          src={require("@/public/top_category/image8@2x.png")}
-          alt="chair"
-          height={400}
-          width={400}
-          className="hover:cursor-pointer w-full object-cover"
-        />
-        <div className="h-[80px] w-full bg-black opacity-70 absolute bottom-0">
-          <div className="text-white my-[18px] ml-4">
-            <p className="font-bold">Wings Chair</p>
-            <p className="font-extralight text-[14px]">3584 Products</p>
-          </div>
+      <div className="px-4 my-10">
+        <h2 className="max-w-screen-xl mx-auto text-3xl font-bold mt-20 mb-10 text-[#272343] text-center lg:text-start">
+          Top Categories
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto max-w-screen-xl">
+          {categories.map((category) => (
+          
+              <div key={category._id}
+                className="relative shadow-md rounded-md hover:scale-105 active:scale-100 duration-500"
+              >
+                <Image
+                  src={category.imageUrl}
+                  alt={category.title}
+                  className="w-full object-cover"
+                  height={400}
+                  width={400}
+                />
+                <div className="h-[80px] w-full bg-black opacity-70 absolute bottom-0 rounded-b-md shadow-md">
+                  <div className="text-white my-[18px] ml-4">
+                    <p className="font-bold">{category.title}</p>
+                    <p>{category.products}</p>
+                  </div>
+                </div>
+              </div>
+           
+          ))}
         </div>
       </div>
-
-      {/* Card 2 */}
-      <div className="relative">
-        <Image
-          src={require("@/public/top_category/image9@2x.png")}
-          alt="chair"
-          height={400}
-          width={400}
-          className="hover:cursor-pointer w-full object-cover"
-        />
-        <div className="h-[80px] w-full bg-black opacity-70 absolute bottom-0">
-          <div className="text-white my-[18px] ml-4">
-            <p className="font-bold">Wooden Chair</p>
-            <p className="font-extralight text-[14px]">157 Products</p>
-          </div>
-        </div>
-      </div>
-
-      {/* Card 3 */}
-      <div className="relative">
-        <Image
-          src={require("@/public/top_category/image10@2x.png")}
-          alt="chair"
-          height={400}
-          width={400}
-          className="hover:cursor-pointer w-full object-cover"
-        />
-        <div className="h-[80px] w-full bg-black opacity-70 absolute bottom-0">
-          <div className="text-white my-[18px] ml-4">
-            <p className="font-bold">Desk Chair</p>
-            <p className="font-extralight text-[14px]">154 Products</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
+    </section>
   );
 };
 
-export default Top_categories;
+export default CategoryListPage;
 
-{/* <section >
-<div>
-  <h2 className="max-w-screen-xl mx-auto text-3xl font-bold mt-20 mb-10 text-[#272343] text-center lg:text-start ">
-    Top Categories
-  </h2>
-  <div className="flex mx-auto max-w-screen-xl flex-col md:flex-row lg:flex-row justify-between">
-    <div className="relative">
-      <Image
-        src={require("@/public/top_category/image8@2x.png")}
-        alt="chair"
-        height={400}
-        width={400}
-        className='hover:cursor-pointer'
-      />
-      <div className="h-[80px] w-full bg-black opacity-70 absolute bottom-0 ">
-        <div className="text-white my-[18px] ml-4">
-          <p className="font-bold">Wings Chair</p>
-          <p className="font-extralight text-[14px]">3584 Products</p>
-        </div>
-      </div>
-    </div>
-    <div className="relative">
-      <Image
-        src={require("@/public/top_category/image9@2x.png")}
-        alt="chair"
-        height={400}
-        width={400}
-        className='hover:cursor-pointer'
-      />
-      <div className="h-[80px] w-full bg-black opacity-70 absolute bottom-0 ">
-        <div className="text-white my-[18px] ml-4">
-          <p className="font-bold">Wooden Chair</p>
-          <p className="font-extralight text-[14px]">157 Products</p>
-        </div>
-      </div>
-    </div>
-    <div className="relative">
-      <Image
-        src={require("@/public/top_category/image10@2x.png")}
-        alt="chair"
-        height={400}
-        width={400}
-        className='hover:cursor-pointer'
-      />
-      <div className="h-[80px] w-full bg-black opacity-70 absolute bottom-0 ">
-        <div className="text-white my-[18px] ml-4">
-          <p className="font-bold">Desk Chair</p>
-          <p className="font-extralight text-[14px]">154 Products</p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-</section> */}
+
